b6dddb599ab3a3f70a3b6698805b23062b8853a2
temevh <temehama@gmail.com>
2025-02-01 12:29:18 +0200

    basic prisma added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import { prisma } from "@/lib/db";
+
+export default async function Home() {
+  const users = await prisma.user.findMany();
+
   return (
     <div className="w-full bg-white">
-      <p>Landing page</p>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p className="text-black">{user.email}</p>
+            <p className="text-black">{user.username}</p>
+            <p className="text-black">{user.password}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
