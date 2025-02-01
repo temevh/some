5fcb1a9085d2dc565fa3e7be0c0c9bd26aa191d4
temevh <temehama@gmail.com>
2025-02-01 12:34:38 +0200

    added link to user on main

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/db";
+import Link from "next/link";
 
 export default async function Home() {
   const users = await prisma.user.findMany();
@@ -9,7 +10,9 @@ export default async function Home() {
         return (
           <div key={user.id}>
             <p className="text-black">{user.email}</p>
-            <p className="text-black">{user.username}</p>
+            <Link className="text-black" href={`/${user.id}`}>
+              {user.username}
+            </Link>
             <p className="text-black">{user.password}</p>
           </div>
         );
